ad39919d31e502e6b1863cff1643f4aeaac17314
laialaodi <107795284+laialaodi@users.noreply.github.com>
2022-06-19 19:19:50 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from douluodalu import DouLuoDaLu5
+
+
+if __name__ == '__main__':
+    link = DouLuoDaLu5()
+    link.run(link=link, mode='run', left=812)
